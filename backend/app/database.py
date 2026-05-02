851943cb1585b72cd7fab46bbab1193846f116b0
Edson Gabriel <edsongabrielbb@gmail.com>
2026-05-02 16:16:47 -0300

    add anotacoes nos arquivos relacionados a db para melhor entendimento

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # A URL de conexão para o Docker rodando na sua máquina
 SQLALCHEMY_DATABASE_URL = "postgresql://admin:123@localhost:5432/safestreets"
 
+# conexao entre os comandos da database e o postgreSQL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# sessao criada para acessar o banco de dados quando um usuário acessar o site
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float
 from app.database import Base
 
+# classe inicial basica para teste
 class Ocorrencia(Base):
     __tablename__ = "ocorrencias"
 
+# atributos da tabela ocorrencias
     id = Column(Integer, primary_key=True, index=True)
     titulo_noticia = Column(String, index=True)
     latitude = Column(Float)
